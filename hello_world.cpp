39a6c2f504f3d32febf949589d4e38270c105692
tonemtonem <kosstylev.ds@gmail.com>
2026-03-11 19:51:12 +0000

    add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
+//the programm displays a greeeting with the users name
 #include <iostream>
 #include <string>
+// using namespace std; deleted for better style
 int main(){
+// declaring a variable to store the username
 std::string name;
+//requesting a username
 std::cout<<"Enter your name: ";
 std::cin>>name;
-std::cout<<"Hellow world from "<<name<<std::endl;
+//greeting output
+std::cout<<"Hello world from "<<name<<std::endl;
 return 0;}
