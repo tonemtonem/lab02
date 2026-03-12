8235470af0c49ce4f8df4e38c505aaee9315a004
tonemtonem <kosstylev.ds@gmail.com>
2026-03-12 17:09:06 +0000

    format code with clang-format (Mozilla style)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,16 @@
-//the programm displays a greeeting with the users name
+// the programm displays a greeeting with the users name
 #include <iostream>
 #include <string>
 // using namespace std; deleted for better style
-int main(){
-// declaring a variable to store the username
-std::string name;
-//requesting a username
-std::cout<<"Enter your name: ";
-std::cin>>name;
-//greeting output
-std::cout<<"Hello world from "<<name<<std::endl;
-return 0;}
+int
+main()
+{
+  // declaring a variable to store the username
+  std::string name;
+  // requesting a username
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  // greeting output
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
+}
